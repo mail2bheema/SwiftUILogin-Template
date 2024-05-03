9a6dfa1a11a61bc51cc7e7c8fea2b1c79a8e6a5c
Bheema Badri <79451254+BheemaBadriMize@users.noreply.github.com>
2024-05-03 10:59:40 +0530

    First commit

---
+++ SwiftUI-Login/SwiftUI_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LoginApp.swift
+//  SwiftUI-Login
+//
+//  Created by Admin on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
